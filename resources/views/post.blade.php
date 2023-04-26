d0d6251cfeee257faf213fe7bb15b8edd203bac4
nguyenconganh <nguyenconganhel97@gmail.com>
2023-04-26 23:20:07 +0700

    update

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -4,16 +4,12 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <title>Laravel111</title>
         <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet" type="text/css">
-        <link href="{{ mix('css/app.css') }}" type="text/css" rel="stylesheet" />
         <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" type="text/css" rel="stylesheet" />
         <meta name="csrf-token" value="{{ csrf_token() }}" />
     </head>
     <body>
         <div id="app">
-            AAAAAAAAAAAAAAAAA
-            BBBBBBBBBBB
-          <!-- <example-component></example-component> -->
         </div>
-        <!-- <script src="{{ mix('js/app.js') }}" type="text/javascript"></script> -->
+        <script src="../js/app.js" type="text/javascript"></script>
     </body>
 </html>
